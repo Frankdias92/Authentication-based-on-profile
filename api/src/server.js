53c35c099d5f1a2506be02919a2ad4977f9bf72b
Franklin Macedo Dias <frankmcdias@gmail.com>
2024-05-15 13:19:12 -0300

    refactor: cookies authentication

--- api/src/server.js
+++ api/src/server.js
@@ -5,10 +5,15 @@ const express = require("express");
 const routes = require("./routes");
 
 const AppError = require("./utils/AppError");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser())
+app.use(cors({
+  origin: ["http://localhost:3333", "http://127.0.0.1:3333"],
+  credentials: true
+}));
 
 app.use(routes);
 
